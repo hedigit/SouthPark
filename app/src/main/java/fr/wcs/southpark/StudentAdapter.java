ba5de8f23a1d2b9bb0de3c308b19cc60c947ab64
Hedi Benaissa <h_benaissa@hotmail.fr>
2018-03-16 10:47:40 +0100

    tag

---
+++ app/src/main/java/fr/wcs/southpark/StudentAdapter.java
@@ -0,0 +1,9 @@
+package fr.wcs.southpark;
+
+/**
+ * Created by wilder on 12/03/18.
+ */
+
+public class StudentAdapter {
+
+}
---
+++ app/src/main/java/fr/wcs/southpark/StudentModel.java
@@ -0,0 +1,28 @@
+package fr.wcs.southpark;
+
+/**
+ * Created by wilder on 12/03/18.
+ */
+
+public class StudentModel {
+
+    String firstname;
+    String lastname;
+
+
+
+    public StudentModel(String firstname, String lastname) {
+       this.firstname = firstname;
+       this.lastname = lastname;
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+}
+
